7c4252704e4b6976d83a16fbfa17a255da81bd3f
VictorSu000 <287764747@qq.com>
2019-06-15 20:30:15 +0800

    add subbank_search

--- API/subbank.py
+++ API/subbank.py
@@ -36,7 +36,7 @@ def subbank_delete(data):
 
 def subbank_update(data):
     r"""
-            :param data: a tuple of data just consists of all 支行 columns
+            :param data: a tuple of data (oldname , newname , city , deposit)
     """
     db = getDB()
     try:
@@ -49,3 +49,27 @@ def subbank_update(data):
         print(e)
         db.rollback()
 
+
+def subbank_search(conditions):
+    r"""
+            :param conditions: a tuple of conditions. each element contains "name" and "condition" fields 
+    """
+    db = getDB()
+    try:
+        cur = db.cursor()
+
+        sql = "SELECT 名字, 城市, 资产 from 支行 where "
+        for con in conditions:
+            sql += f"{con['name']} {con['condition']} and "
+        
+        if sql[-4:] != "and":
+            # 没有任何条件，去除最后的 "where "
+            sql = sql[:-6]
+        else:
+            # 去除多余的 "and "
+            sql = sql[:-4]
+        cur.execute(sql)
+        return cur.fetchall()
+
+    except Exception as e:
+        print(e)
